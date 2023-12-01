eec7059d0d52028b35b8f9c72b30de1072e4bb73
Caio Alex <128102270+Furianoite@users.noreply.github.com>
2023-12-01 19:41:14 -0300

    Update estrutura.php

--- estrutura.php
+++ estrutura.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>Análise Léxica de Código</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
     <style>
         body {
             display: flex;
@@ -10,6 +9,7 @@
             align-items: center;
             height: 100vh;
             margin: 0;
+            background: linear-gradient(to bottom right, #333333, #666666);
         }
         .container {
             max-width: 600px;
@@ -20,39 +20,80 @@
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); 
         }
+        .code-label {
+            font-family: Arial, sans-serif;
+            font-weight: bold;
+            font-size: 25px; 
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 10px; 
+            background: linear-gradient(to right, #333333, #999999);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+        textarea {
+            margin-top: 10px;
+            padding: 8px;
+            border-radius: 5px;
+            border: 2px solid #ccc;
+            font-size: 16px;
+            font-family: 'Arial';
+        }
+        .custom-select {
+            margin-top: 10px;
+        }
+        input[type="submit"] {
+            margin-top: 10px;
+            padding: 8px 15px;
+            border-radius: 5px;
+            border: none;
+            background-color: #4CAF50;
+            color: white;
+            font-size: 16px;
+            font-weight: bold;
+        }
+        .resultado-analise {
+            display: none;
+            margin-top: 20px;
+            padding: 10px;
+            border: 2px solid #ccc;
+            border-radius: 5px;
+            text-align: left;
+        }
+        h2 {
+            margin-bottom: 10px;
+        }
+        strong {
+            font-size: 18px;
+        }
     </style>
 </head>
 <body>
     <div class="container">
-        <div class="logo-container">
-        <img src="https://d9hhrg4mnvzow.cloudfront.net/lp.fpb.edu.br/fpb-formas-de-entrada/f338a297-fpb-logo-branco-sem-lau_105f014000000000000028.png" class="logo" alt="Logo animada">
-    </div>
+        <h1 class="code-label">ANALISADOR LÉXICO</h1>
 
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <label for="codigo" class="code-label">INSIRA O CÓDIGO:</label><br>
-        <textarea id="codigo" name="codigo" rows="10" cols="50"></textarea><br>
-        <label for="linguagem" class="select-label">Selecione a linguagem:</label>
-        <div class="custom-select">
-            <select name="linguagem" id="linguagem">
-                <option value="php">PHP</option>
-                <option value="python" >PYTHON</option>
-                <option value="java">JAVA</option>
-            </select>
-        </div>
-        <input type="submit" value="Analisar">
-    </form>
-</body>
-</html>
-
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label for="codigo" class="code-label">INSIRA O CÓDIGO:</label><br>
+            <textarea id="codigo" name="codigo" rows="10" cols="50"></textarea><br>
+            <label for="linguagem" class="select-label">Selecione a linguagem:</label>
+            <div class="custom-select">
+                <select name="linguagem" id="linguagem">
+                    <option value="php">PHP</option>
+                    <option value="python">PYTHON</option>
+                    <option value="java">JAVA</option>
+                </select>
+            </div>
+            <input type="submit" value="Analisar">
+        </form>
 <?php
 function realizarAnaliseLexica($codigo, $linguagem) {
-    
+
     $padraoPalavrasChave = '';
     $padraoIdentificadores = '';
     $padraoOperadores = '';
     $padraoDelimitadores = '';
 
-   
     switch ($linguagem) {
         case 'php':
             $padraoPalavrasChave = "/\b(if|else|for|while|do|break|class|public|private|function|return|try|catch|finally)\b/";
@@ -62,37 +103,37 @@ function realizarAnaliseLexica($codigo, $linguagem) {
             break;
         case 'python':
             $padraoPalavrasChave = "/\b(if|else|for|while|def|class|import|elif|try|except|finally)\b/";
-            $padraoIdentificadores = "/\b[a-zA-Z_][a-zA-Z0-9_]*\b/";
+            $padraoIdentificadores = "/\b(?!(if|else|for|while|print|def|class|import|elif|try|except|finally))\b[a-zA-Z_][a-zA-Z0-9_]*\b/";
             $padraoOperadores = "/[\+\-\*\/=<>]|and|or|not/";
             $padraoDelimitadores = "/[\(\)\{\}\[\]\.,;]/";
-            break;            
+            break;
         case 'java':
             $padraoPalavrasChave = "/\b(if|else|for|while|do|break|class|public|private|static|void|interface|extends|implements|this|super)\b/";
-            $padraoIdentificadores = "/\b[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*\b/";
+            $padraoIdentificadores = "/\b(?!(if|else|for|while|do|break|class|public|private|static|void|interface|extends|implements|this|super|System|out|println|printStream))\b[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*\b/";
             $padraoOperadores = "/[\+\-\*\/=<>]|&&|\|\|/";
             $padraoDelimitadores = "/[\(\)\{\}\[\]\.,;]/";
-            break;
+    break;
         default:
             echo "Linguagem não suportada.";
             return;
     }
+            
+    $codigoSemStrings = preg_replace('/".*?"/', '', $codigo);
 
+    preg_match_all($padraoIdentificadores, $codigoSemStrings, $identificadoresEncontrados);
     preg_match_all($padraoPalavrasChave, $codigo, $palavrasChaveEncontradas);
     preg_match_all($padraoIdentificadores, $codigo, $identificadoresEncontrados);
     preg_match_all($padraoOperadores, $codigo, $operadoresEncontrados);
     preg_match_all($padraoDelimitadores, $codigo, $delimitadoresEncontrados);
 
-    $identificadoresFiltrados = array_filter($identificadoresEncontrados[0], function ($identificador) use ($padraoPalavrasChave) {
-        return !preg_match($padraoPalavrasChave, $identificador);
-    });
-
-    $identificadoresUnicos = array_unique($identificadoresFiltrados);
+    $palavrasChaveUnicas = array_unique($palavrasChaveEncontradas[0]);
+    $identificadoresUnicos = array_unique($identificadoresEncontrados[0]);
     $operadoresUnicos = array_unique($operadoresEncontrados[0]);
     $delimitadoresUnicos = array_unique($delimitadoresEncontrados[0]);
-   
+
     echo '<div class="resultado-analise" id="resultado-analise">';
     echo "<h2>Resultado da Análise Léxica:</h2>";
-    echo "<strong>Palavras-chave encontradas:</strong> " . implode(", ", $palavrasChaveEncontradas[0]) . "<br>";
+    echo "<strong>Palavras-chave encontradas:</strong> " . implode(", ", $palavrasChaveUnicas) . "<br>";
     echo "<strong>Operadores encontrados:</strong> " . implode(", ", $operadoresUnicos) . "<br>";
     echo "<strong>Delimitadores encontrados:</strong> " . implode(", ", $delimitadoresUnicos) . "<br>";
     echo "<strong>Identificadores (variáveis) encontrados:</strong> " . implode(", ", $identificadoresUnicos) . "<br>";
@@ -105,7 +146,6 @@ function realizarAnaliseLexica($codigo, $linguagem) {
         }, 1000);
     </script>';
 }
-
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["codigo"]) && isset($_POST["linguagem"])) {
     $codigoInserido = $_POST["codigo"];
